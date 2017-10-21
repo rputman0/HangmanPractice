870753ba12412c965b7cfba34e3fd2c8488232eb
rputman0 <rputman0@gmail.com>
2017-10-21 16:53:17 -0400

    Update HangMan.py
    -rename method functions and variable names
    -create getWord(),isSpecialCharacter(letter),toString(guessList),isEqualTo(guess,word)
    -move and update comments
    -clean if statements

--- HangMan.py
+++ HangMan.py
@@ -1,52 +1,69 @@
 from random import randint
 import linecache
 
-def createDashes(string):
-    dashes = ""
-    for i in range(len(string)):
-        if(string[i] == " "):
-            dashes += " "
-        else:
-            dashes += "-"
-
-    return dashes
-
+#read from text file to get random movie title
 def getWord():
     fileName = ("movies.txt")
-    movieNumber = randint(0,403)
+    movieNumber = randint(0,403) #the length of the file
     word = linecache.getline(fileName,movieNumber)
     word = word.rstrip('\n')
 
     return word
 
+#abstract the word to the player
+def convertToDashes(word):
+    dashes = ""
+    for i in range(len(word)):
+        if(isSpecialCharacter(word[i])):
+            dashes += word[i]
+        else:
+            dashes += "-"
+
+    return dashes
+
+def isSpecialCharacter(letter):
+    isSpecial = False
+    specialCharacters = " '!.,/&[]():0123456789"
+    for i in range(len(specialCharacters)):
+        if(specialCharacters[i] == letter):
+            isSpecial = True
+            break
+
+    return isSpecial
+
+def toString(guessList):
+    return "".join(guessList)
+
+def isEqualTo(guess,word):
+    return guess == word
+
 def main():
-    print("HANGMAN - Guess the movie title in FIVE tries")
-
-    #read from text file to get random movie title
-    word = getWord()
+    print("HANGMAN - Guess the movie title in SIX tries")
+    count,tries = 0,0
+    wrong = ""
+    isInWord = False
     
-    #abstract the word to the player
-    dashes = createDashes(word)
+    word = getWord()
+    dashes = convertToDashes(word)
+    print(word)
     print(dashes)
-
+    
     #python strings are immutable, use list() to change strings in place
     guess = list(dashes)
 
-    count = 0
-    tries = 0
-    wrong = ""
-    isInWord = False
-
     #let the user guess
-    while("".join(guess) != word and tries  <= 5):
-        userGuess = input("User Guess: ");
-
-        #let user guess the entire word
-        if(len(userGuess) > 1 and userGuess == word):
+    while( not isEqualTo(guess,word) and tries <= 5):
+        userGuess = input("User Guess: ")
+            
+        #let user guess the entire word, or just a letter
+        if(len(userGuess) > 1 and isEqualTo(userGuess,word)):
             guess = word
             break
-        else:
+        elif(len(userGuess) == 1):
             letter = userGuess[0]
+        else:
+            letter = " " #catches wrong input
+            print("Please guess a character or movie title\n")
 
         #check if the letter is in the word
         for i in range(len(word)):
@@ -66,12 +83,14 @@ def main():
             print("Correct Letter")
             isInWord = False #set the boolean back to false
 
-        print("".join(guess))
+        print(toString(guess))
 
     #Tell the player if he won or lost
     if(tries >= 5):
         print("You LOST! THE WORD WAS:",word)
-    elif("".join(guess) == word):
+    elif( isEqualTo(guess,word) and tries == 0):
+        print("You WON WITH ZERO MISTAKES!!! THE WORD WAS:",word)
+    elif( isEqualTo(guess,word) ):
         print("You Win!!! THE WORD WAS:",word)
         print("You had",tries,"mistakes")
 
